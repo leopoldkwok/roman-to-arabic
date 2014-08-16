2d769b1721bb1650636d2eb0f041e9bccf5a1b1b
David Wickes <dave.wickes@gmail.com>
2014-08-16 22:38:17 +0100

    trying as a monkey patch. Tested up to 4

--- lib/roman_numeral_parser.rb
+++ lib/roman_numeral_parser.rb
@@ -1,31 +1,11 @@
-class RomanNumerals
+class String
 
-	def self.roman_numerals_parser(number)
-		times_I = number.count('I')
-		times_V = number.count('V')
-		times_X = number.count('X')
-		times_L = number.count('L')
-		times_C = number.count('C')
-		times_D = number.count('D')
-		times_M = number.count('M')
-		less_I  = number.scan(/I[^I]/).count
-		less_X  = number.scan(/X[^XVI]/).count
-		less_C  = number.scan(/C[MD]/).count
-		#less_CM  = number.scan(/M[C]/).count
+	def arabic
+	
+		fours = self.scan(/IV/).count
 
-		result = ( 1 * times_I ) +
-						 ( 5 * times_V ) +
-						 ( 10 * times_X ) +
-						 ( 50 * times_L ) +
-						 ( 100 * times_C ) +
-						 ( 500 * times_D) +
-						 ( 1000 * times_M) + 
-						 ( less_I * -2 ) +
-						 ( less_X * -20 )+
-						 ( less_C * -200) 
-					#	 ( less_CM * - 200)
-
-		result
+		self.count('I') +
+		(self.count('V') * 5) +
+		(fours * -2)
 	end
-
 end
--- spec/roman_numeral_parser_spec.rb
+++ spec/roman_numeral_parser_spec.rb
@@ -1,63 +1,23 @@
 require 'roman_numeral_parser'
 
-describe RomanNumerals do
+describe 'a roman numeral string' do
 
-	it "should know that I is 1" do
-		expect(RomanNumerals.roman_numerals_parser('I')).to eq 1
+	it "of I becomes 1" do
+		expect('I'.arabic).to be 1
 	end
 
-	it "should know that II is 2" do
-		expect(RomanNumerals.roman_numerals_parser('II')).to eq 2
+	it "of II becomes 2" do
+		expect('II'.arabic).to be 2
 	end
 
-	it "should know that III is 3" do
-		expect(RomanNumerals.roman_numerals_parser('III')).to eq 3
+	it "of V becomes 5" do
+		expect('V'.arabic).to be 5
 	end
 
-	it "should know that X is 10" do
-		expect(RomanNumerals.roman_numerals_parser('X')).to eq 10
+	it "of IV becomes 4" do
+		expect('IV'.arabic).to be 4
 	end
 
-	it "should know that L is 50" do
-		expect(RomanNumerals.roman_numerals_parser('L')).to eq 50
-	end
-
-	it "should know that C is 100" do
-		expect(RomanNumerals.roman_numerals_parser('C')).to eq 100
-	end
-
-	it "should know that IV is 4" do
-		expect(RomanNumerals.roman_numerals_parser('IV')).to eq 4
-	end
-
-	it "should know that IX is 9" do
-		expect(RomanNumerals.roman_numerals_parser('IX')).to be 9
-	end
-
-	it "should know that XXV is 25" do
-		expect(RomanNumerals.roman_numerals_parser('XXV')).to eq 25
-	end
-
-	it "should know that XL is 40" do
-		expect(RomanNumerals.roman_numerals_parser('XL')).to eq 40
-	end
-
-	it "should know that XCIX is 99" do
-		expect(RomanNumerals.roman_numerals_parser('XCIX')).to eq 99
-	end
-
-	it "should know that CD is 400" do
-		expect(RomanNumerals.roman_numerals_parser('CD')).to eq 400
-	end
-
-	it "should know that M is 1000" do
-		expect(RomanNumerals.roman_numerals_parser('M')).to eq 1000
-	end
-
-	it "should know that CM is 900" do
-		expect(RomanNumerals.roman_numerals_parser('CM')).to eq 900
-	end
-
-
+	
 
 end
