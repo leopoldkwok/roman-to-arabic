ae7e2ea6d0b591159b6b9d921c52ae5f3e124ab9
Henry Stanley <henry@henrystanley.com>
2014-08-16 13:55:19 +0100

    Added failing test for II -> 2

--- spec/roman_numeral_parser_spec.rb
+++ spec/roman_numeral_parser_spec.rb
@@ -5,5 +5,9 @@ describe RomanNumerals do
 	it "should know that I is 1" do
 		expect(RomanNumerals.roman_numerals_parser('I')).to eq 1
 	end
+
+	it "should know that II is 2" do
+		expect(RomanNumerals.roman_numerals_parser('II')).to eq 2
+	end
 	
 end
